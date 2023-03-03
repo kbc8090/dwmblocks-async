f1183906335e354daa2ed58df70a3120dd3aac15
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2023-03-02 21:13:27 -0500

    Add files via upload

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 #define CMDLENGTH 45
-#define DELIMITER " "
+#define DELIMITER " "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
 	BLOCK("~/.local/bin/weather",    1800, 17),
-	BLOCK("~/.local/bin/pacupdate",   1800,    18),
+	BLOCK("~/.local/bin/pacupdate",   2000,    18),
 	BLOCK("~/.local/bin/volume",    0, 19),
-	BLOCK("~/.local/bin/kernel",    43200, 0),
-	BLOCK("~/.local/bin/memory",  5,   20),
-	BLOCK("~/.local/bin/clock",    5,    24)
+	BLOCK("~/.local/bin/kernel",    43200, 20),
+	BLOCK("~/.local/bin/memory",  3,   21),
+	BLOCK("~/.local/bin/clock",    3,    24)
 };
